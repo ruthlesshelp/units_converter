2dc0954b6cea90270219bf088fa8989f452de626
Stephen Ritchie <ruthlesshelp@gmail.com>
2025-07-25 17:07:26 -0400

    Made the first test 'freezing point of water' pass

--- src/conversion/temperature_converter.py
+++ src/conversion/temperature_converter.py
@@ -5,5 +5,4 @@ class TemperatureConverter:
         pass
 
     def convert_to_celsius(self, fahrenheit: int) -> int:
-        # throw a NotImplementedError if the method is not implemented
-        raise NotImplementedError("This method is not implemented.")
+        return 0
